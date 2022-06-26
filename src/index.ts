9ae1766fe6623dac6f6f80d7c8c5b57598571582
Denis Brazhnikov <brazhnikov.d@outlook.com>
2022-06-26 20:01:59 +0300

    feat: implement env handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import {httpServer} from './server/http';
-import './server/websocket';
+import {wsServer} from './server/websocket';
+import {config} from 'dotenv';
+import {join} from 'path';
 
-httpServer.listen(8000, () => {
-    console.info(`HTTP server is running on port 8000`);
+const env = config({path: join(__dirname, '..', '.env')});
+
+const wsPort = parseInt(String(env.parsed?.WEBSOCKET_PORT)) || 8080;
+const httpPort = parseInt(String(env.parsed?.HTTP_PORT)) || 8000;
+
+wsServer(wsPort).then(_ => {
+});
+
+httpServer.listen(httpPort, () => {
+    console.info(`HTTP server is running on port ${httpPort}`);
 })
--- src/server/websocket/index.ts
+++ src/server/websocket/index.ts
@@ -1,12 +1,14 @@
 import {WebSocketServer} from 'ws';
 import {message} from './message';
 
-const ws = new WebSocketServer({
-    port: 8080,
-});
+export const wsServer = async (port: number) => {
+    const ws = new WebSocketServer({
+        port: port,
+    });
 
-ws.on('connection', message);
+    ws.on('connection', message);
 
-ws.on('listening', () => {
-    console.info(`Websocket server is running on port 8080`);
-});
+    ws.on('listening', () => {
+        console.info(`Websocket server is running on port ${port}`);
+    });
+}
